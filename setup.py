04a6fd59fed3c0721b392d09d029d69077bedabe
DESKTOP-83H17MI\kbulls <kbulls99@gmail.com>
2018-05-10 02:27:00 +0900

    fix error

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@
 ### Do not forget to adjust the following variables to your own plugin.
 
 # The plugin's identifier, has to be unique
-plugin_identifier = "enclosure_opi"
+plugin_identifier = "enclosure"
 
 # The plugin's python package, should be "octoprint_<plugin identifier>", has to be unique
-plugin_package = "octoprint_enclosure_opi"
+plugin_package = "octoprint_enclosure"
 
 # The plugin's human readable name. Can be overwritten within OctoPrint's internal data via __plugin_name__ in the
 # plugin module
